162ec65bb9823e746bed60a8c45fe566719e1ade
ahals <ahalyag96@gmail.com>
2019-08-31 13:11:36 +0530

    ENd statement missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
     render html:'<html><body>HELLO WORLD</body></html>'.html_safe
+  end
 end
